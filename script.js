0ea5c862e1cb7d8cf2188617f2f40581802c6d0d
CutelilCucumber <sond.brian@gmail.com>
2025-08-29 17:06:40 -0700

    add basis async promise functionality

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 const fetchButt = document.querySelector('button');
 const cityInput = document.querySelector('input');
+let currentCity = '';
+let cityList = [];
 
 //function to take location and return weather data
-async function getWeather(location){//call asycn function for await fetch
+async function getWeather(){//call asycn function for await fetch
 
     try {//try method to test for invalid response
         //use await to assign fetch promise to var
-        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Portland?key=7776AA8FBBF6VLG9ZUASC8QK5', {mode: 'cors'});
+        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + currentCity + '?key=7776AA8FBBF6VLG9ZUASC8QK5', {mode: 'cors'});
         //use await to assign .json method promise to var
         const weatherData = await response.json();
-        return weatherData;
+        return extractData(weatherData);
     }
     catch (error) {//catch errors and return it
         console.error(error);
@@ -18,4 +20,15 @@ async function getWeather(location){//call asycn function for await fetch
     }
 }
 
-console.log(getWeather());
+function extractData(weatherData) {
+    const dataObject = weatherData;
+    return dataObject;
+}
+
+//event listener to call getweather and record the location on button click
+fetchButt.addEventListener('click', () => {
+    currentCity = cityInput.value;
+    console.log(getWeather());
+
+    //update visually
+})
